557e8a287efe947b437c1d85df6fac2a9ad8709b
l-Alli-l <90545625+agattone96@users.noreply.github.com>
2025-09-08 11:46:28 -0400

    feat: enable dialing from hero phone button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,13 +35,16 @@ const Hero = () => {
               >
                 Get Free Estimate
               </Button>
-              <Button 
-                variant="outline" 
+              <Button
+                asChild
+                variant="outline"
                 size="lg"
                 className="border-white/30 text-white hover:bg-white/10 transition-smooth text-lg px-8 py-4 font-semibold"
               >
-                <Phone className="w-5 h-5 mr-2" />
-                {FORMATTED_PHONE_NUMBER}
+                <a href="tel:7276214041">
+                  <Phone className="w-5 h-5 mr-2" />
+                  {FORMATTED_PHONE_NUMBER}
+                </a>
               </Button>
             </div>
 
